9b14587da8dda01cb6b778ac65beff502f0912fc
Mike Chen <mcyc@users.noreply.github.com>
2022-05-10 15:43:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mufasa", # Replace with your own username
-    version="0.2.0",
+    name="mufasa", 
+    version="1.0.0",
     author="Michael Chun-Yuan Chen",
-    author_email="mcychen@uvic.ca",
+    author_email="chen.m@queensu.ca",
     description="MUlti-component Fitter for Astrophysical Spectral Applications",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mcyc/mufasa",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=2.7',
+    python_requires='>=3.6',
 )
