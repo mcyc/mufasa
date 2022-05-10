f7dfef78820dffa8893aca1bc48661fa02522b3f
Mike Chen <mkid.chen@gmail.com>
2022-05-10 16:13:01 -0400

    Revert "Update setup.py"
    This reverts commit b90813fe60d40404adc55ac5ff1c72f290bf3cb0.

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mufasa", 
-    version="1.0.0",
+    name="mufasa", # Replace with your own username
+    version="0.2.0",
     author="Michael Chun-Yuan Chen",
-    author_email="chen.m@queensu.ca",
+    author_email="mcychen@uvic.ca",
     description="MUlti-component Fitter for Astrophysical Spectral Applications",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mcyc/mufasa",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=2.7',
 )
