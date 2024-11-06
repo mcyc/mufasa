53e4f1eadb70ca79531da0b4f397e8136b68027f
Mike Chen <mcyc@users.noreply.github.com>
2024-11-06 15:10:27 -0500

    Fixed a referencing bug

--- mufasa/master_fitter.py
+++ mufasa/master_fitter.py
@@ -467,12 +467,12 @@ def master_2comp_fit(reg, snr_min=0.0, recover_wide=True, planemask=None, update
         else:
             max_iter = 25
 
-        n_good_total = mf.expand_fits(reg, ncomp=1, lnk_thresh=10, max_iter=max_iter, r_expand=1, save_para=True)
-        n_good_total = mf.expand_fits(reg, ncomp=2, lnk_thresh=10, max_iter=max_iter, r_expand=1, save_para=True)
-        mf.refit_marginal(reg, 1, lnk_thresh=5, multicore=True, save_para=True)
-        mf.refit_marginal(reg, 2, lnk_thresh=5, multicore=True, save_para=True)
-        n_good_total = mf.expand_fits(reg, ncomp=1, lnk_thresh=5, max_iter=max_iter, r_expand=1, save_para=True)
-        n_good_total = mf.expand_fits(reg, ncomp=2, lnk_thresh=5, max_iter=max_iter, r_expand=1, save_para=True)
+        n_good_total = expand_fits(reg, ncomp=1, lnk_thresh=10, max_iter=max_iter, r_expand=1, save_para=True)
+        n_good_total = expand_fits(reg, ncomp=2, lnk_thresh=10, max_iter=max_iter, r_expand=1, save_para=True)
+        refit_marginal(reg, 1, lnk_thresh=5, multicore=True, save_para=True)
+        refit_marginal(reg, 2, lnk_thresh=5, multicore=True, save_para=True)
+        n_good_total = expand_fits(reg, ncomp=1, lnk_thresh=5, max_iter=max_iter, r_expand=1, save_para=True)
+        n_good_total = expand_fits(reg, ncomp=2, lnk_thresh=5, max_iter=max_iter, r_expand=1, save_para=True)
 
     save_best_2comp_fit(reg, multicore=multicore, lnk21_thres=5, lnk10_thres=5)
 
