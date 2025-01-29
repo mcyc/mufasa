b6f231bcb244d244c1768ea4965452a1ca43bd9e
Mike Chen <mcyc@users.noreply.github.com>
2025-01-28 20:24:42 -0500

    WIP: started overhauling nh3 model using the BaseModel class

--- mufasa/spec_models/ammonia_multiv.py
+++ mufasa/spec_models/ammonia_multiv.py
@@ -9,6 +9,7 @@ from pyspeckit.spectrum.models import model
 from pyspeckit.spectrum.models.ammonia_constants import (line_names, freq_dict, voff_lines_dict, tau_wts_dict)
 from pyspeckit.spectrum.models.ammonia_constants import (ckms, h, kb)
 
+from .BaseModels import BaseModel
 from .SpecModels import AmmoniaModel
 
 #=======================================================================================================================
